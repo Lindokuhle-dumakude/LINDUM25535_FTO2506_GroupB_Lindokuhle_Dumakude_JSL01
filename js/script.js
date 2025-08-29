51f2a946c8a66a47e36bb7f9f64c4b751e0d7171
unknown <lindokuhle.dumakude11@gmail.com>
2025-08-29 13:26:25 +0200

    declare variables for 2 different tasks.

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Declaring task 1 variables
+
+let task1Title;
+let task1Description;
+let task1Status;
+
+// Declaring task 2 variables
+
+let task2Title;
+let task2Description;
+let task2Status;
